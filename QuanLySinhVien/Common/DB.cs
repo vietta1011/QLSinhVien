8c2db60ad5b22e526f22f7935c22b4af9c0d752d
vietta1011 <taquocviet1011@gmail.com>
2023-12-02 13:05:48 +0700

    first commit

---
+++ QuanLySinhVien/Common/DB.cs
@@ -0,0 +1,7 @@
+﻿namespace QuanLySinhVien.Common
+{
+    public static class DB
+    {
+        public static string ConnectString = "Data Source=WAVYZ-LAPTOP\\VIET;Initial Catalog=QuanLySinhVien;Integrated Security=True";
+    }
+}
